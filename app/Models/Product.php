6a1be55da01fc322736b69feb523b7adc768eae9
AngelaC34 <149558767+AngelaC34@users.noreply.github.com>
2024-06-11 00:12:00 +0700

    create product models and controller

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'product_category',
+        'product_price',
+        'product_image',
+        'product_description',
+        'product_created',
+        'product_updated',
+    ];
+
+    protected $table = 'Product';
+
+}
