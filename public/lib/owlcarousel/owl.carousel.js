b87da6d534fafcaff52d9610097d02b242bb484c
wontonsouper <149463477+wontonsouper@users.noreply.github.com>
2024-06-10 18:57:10 +0700

    update lib

--- public/lib/owlcarousel/owl.carousel.js
+++ public/lib/owlcarousel/owl.carousel.js
@@ -1,25 +1,18 @@
-/**
- * Owl Carousel v2.2.1
- * Copyright 2013-2017 David Deutsch
- * Licensed under  ()
- */
+
 /**
  * Owl carousel
  * @version 2.1.6
- * @author Bartosz Wojciechowski
- * @author David Deutsch
- * @license The MIT License (MIT)
- * @todo Lazy Load Icon
- * @todo prevent animationend bubling
- * @todo itemsScaleUp
- * @todo Test Zepto
- * @todo stagePadding calculate wrong active classes
+ * @todo 
+ * @todo 
+ * @todo 
+ * @todo 
+ * @todo 
  */
 ;(function($, window, document, undefined) {
 
 	/**
 	 * Creates a carousel.
-	 * @class The Owl Carousel.
+	 * @class
 	 * @public
 	 * @param {HTMLElement|jQuery} element - The element to create the carousel for.
 	 * @param {Object} [options] - The options
