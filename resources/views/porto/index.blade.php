dcc4ece62154a7099cdf7b07340183d94c3ae582
wontonsouper <149463477+wontonsouper@users.noreply.github.com>
2024-06-10 18:39:22 +0700

    update lib

--- resources/views/porto/index.blade.php
+++ resources/views/porto/index.blade.php
@@ -88,28 +88,28 @@
 
     <!-- Header Start -->
     <div class="container-fluid bg-dark p-0 mb-5">
-    <div class="row g-0 flex-column-reverse flex-lg-row">
-        <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
-            <div class="owl-carousel header-carousel">
-                <div class="owl-carousel-item">
-                    <img class="img-fluid" src="{{ asset('images/menus/Porto_s Big Breakfast.jpeg') }}" alt="">
-                </div>
-                <div class="owl-carousel-item">
-                    <img class="img-fluid" src="{{ asset('images/menus/Creamy Ginger Linguine.JPG') }}" alt="">
-                </div>
-                <div class="owl-carousel-item">
-                    <img class="img-fluid" src="{{ asset('images/menus/Nachos.jpg') }}" alt="">
-                </div>
-                <div class="owl-carousel-item">
-                    <img class="img-fluid" src="{{ asset('images/menus/khewnyuk.jpg') }}" alt="">
-                </div>
-                <div class="owl-carousel-item">
-                    <img class="img-fluid" src="{{ asset('images/menus/Kue Lobak.jpg') }}" alt="">
+        <div class="row g-0 flex-column-reverse flex-lg-row">
+            <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                <div class="owl-carousel header-carousel">
+                    <div class="owl-carousel-item">
+                        <img class="img-fluid" src="{{ asset('images/menus/Porto_s Big Breakfast.jpeg') }}" alt="">
+                    </div>
+                    <div class="owl-carousel-item">
+                        <img class="img-fluid" src="{{ asset('images/menus/Creamy Ginger Linguine.JPG') }}" alt="">
+                    </div>
+                    <div class="owl-carousel-item">
+                        <img class="img-fluid" src="{{ asset('images/menus/Nachos.jpg') }}" alt="">
+                    </div>
+                    <div class="owl-carousel-item">
+                        <img class="img-fluid" src="{{ asset('images/menus/khewnyuk.jpg') }}" alt="">
+                    </div>
+                    <div class="owl-carousel-item">
+                        <img class="img-fluid" src="{{ asset('images/menus/Kue Lobak.jpg') }}" alt="">
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
     <!-- Header End -->
 
