e803b3b3657df5c3a30a0fa118693bf2fbac7b26
AngelaC34 <149558767+AngelaC34@users.noreply.github.com>
2024-06-12 10:36:41 +0700

    product create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -20,6 +20,18 @@ class ProductController extends Controller
         return view('product.create');
     }
 
-
+    // Store method for Product
+    public function store(Request $request) {
+        $request->validate([
+            'product_name' => 'required',
+            'product_category' => 'required',
+            'product_price' => 'required|integer',
+            'product_image' => 'required',
+            'product_description' => 'required',
+        ]);
+        $product = Product::create($request->all());
+        return redirect()->route('product')
+            ->with('success', 'Product created successfully.');
+    }
 
 }
