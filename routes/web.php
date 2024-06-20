5a4fd96829be66d0cfc0518ef68906c9fa65ad40
wontonsouper <149558071+ChristabellaJC@users.noreply.github.com>
2024-06-20 17:56:50 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ReviewController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CheckoutController;
 
 Auth::routes();
 
@@ -29,6 +30,7 @@ Route::get('/index', [HomeController::class, 'index'])->name('index');
 Route::get('/menus', [MenusController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
 Route::get('/orderpage', [OrderController::class, 'index']);
+Route::get('/checkout', [CheckoutController::class, 'index']);
 Route::get('/home', [AdminController::class, 'index'])->name('home');
 
 Route::get('/review/create', [ReviewController::class, 'create'])->name('review.create');
