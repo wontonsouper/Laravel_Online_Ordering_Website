6d427be9d6905a393204b93904a78ef2ceb38bfb
535220166 <Christabella.535220166@stu.untar.ac.id>
2024-06-26 00:51:00 +0700

    Update index.blade.php

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -4,14 +4,14 @@
 <div class="container-fluid py-5">
     <div class="container py-5">
         <h1 class="mb-4">All Orders</h1>
-        <table class="table table-bordered">
+        <table class="table">
             <thead>
                 <tr>
-                    <th>Order ID</th>
-                    <th>Table</th>
-                    <th>Total</th>
-                    <th>Status</th>
-                    <th>Details</th>
+                    <th scope="col">Order ID</th>
+                    <th scope="col">Table</th>
+                    <th scope="col">Total</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -20,8 +20,18 @@
                     <td>{{ $order->id }}</td>
                     <td>{{ $order->order_table }}</td>
                     <td>Rp. {{ number_format($order->order_total, 0, ',', '.') }}</td>
-                    <td>{{ ucfirst($order->order_status) }}</td>
-                    <td><a href="{{ route('orders.show', $order->id) }}" class="btn btn-info">View</a></td>
+                    <td>
+                        <form action="{{ route('order.update', $order->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('put')
+                            <input type="radio" name="order_status" value="pending" {{ $order->order_status == 'pending' ? 'checked' : '' }}> Pending
+                            <input type="radio" name="order_status" value="completed" {{ $order->order_status == 'completed' ? 'checked' : '' }}> Completed
+                            <button type="submit" class="btn btn-primary">Update</button>
+                        </form>
+                    </td>
+                    <td>
+                        <a href="{{ route('order.show', $order->id) }}" class="btn btn-info">View Order</a>
+                    </td>
                 </tr>
                 @endforeach
             </tbody>
